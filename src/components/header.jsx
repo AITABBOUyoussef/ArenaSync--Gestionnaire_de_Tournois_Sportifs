e5fe8f242dc323ea96f231a2424fa6d0d9ec0154
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-03-06 12:24:52 +0000

    Refactor Header component layout and styling for improved UI

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      <div>
-        <h1>Good Morning,Samuel Walker!</h1>
-        <div></div>
-        <img src="" alt="" />
-      </div>
+    <div className='bg-blue-500'>
+      <div className='flex  justify-between p-2'>
+        <h1 className='w-40  text-xl font-bold text-white '>Good Morning, Samuel Walker!</h1>
+        <div className='flex gap-4'><div className='rounded-full bg-white/50 w-14 h-12 flex justify-center items-center'><i class="fa-regular fa-bell" ></i></div>
+        <img className='w-14 h-12 rounded-full' src="https://images.pexels.com/photos/36184064/pexels-photo-36184064.jpeg?_gl=1*r5z7xl*_ga*MTkyNDkzMjI0MS4xNzcyNzk1NTUw*_ga_8JE65Q40S6*czE3NzI3OTc2NjAkbzIkZzEkdDE3NzI3OTkzNjckajQwJGwwJGgw" alt="" />
+      </div></div>
       <div>
         <input type="text" />
       </div>
